3ca931be71dea5f112ca1ef85221af8b57bf75d0
allanmurimi <allanmurimi@gmail.com>
2021-09-20 17:37:34 +0300

    made hover and click on image and icon individual

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
-$(function($){
-    $('.what-img').on("click",function(event){
-        $('.what-content-1').toggle();
-        $('.what-content-2').toggle();
-        $('.what-content-3').toggle();
-    })
-})
+$(function ($) {
+  $(".what-img-1").on("click", function (event) {
+    $(".what-content-1").toggle();
+  });
+});
 
-$(function($){
-    $('.portfolio-img').hover(function(){
-        $('.content-img').toggle();
-    })
-})
+$(function ($) {
+  $(".what-img-2").on("click", function (event) {
+    $(".what-content-2").toggle();
+  });
+});
+
+$(function ($) {
+  $(".what-img-3").on("click", function (event) {
+    $(".what-content-3").toggle();
+  });
+});
+
+$(function ($) {
+  $(".portfolio-img-0").hover(function () {
+    $(".content-img-0").toggle();
+  });
+});
+
+$(function ($) {
+  $(".portfolio-img-00").hover(function () {
+    $(".content-img-00").toggle();
+  });
+});
+
+$(function ($) {
+  $(".portfolio-img-1").hover(function () {
+    $(".content-img-1").toggle();
+  });
+});
+
+$(function ($) {
+  $(".portfolio-img-2").hover(function () {
+    $(".content-img-2").toggle();
+  });
+});
+
+$(function ($) {
+  $(".portfolio-img-3").hover(function () {
+    $(".content-img-3").toggle();
+  });
+});
+
+$(function ($) {
+  $(".portfolio-img-4").hover(function () {
+    $(".content-img-4").toggle();
+  });
+});
